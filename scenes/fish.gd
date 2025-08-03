fbd43b42617ff8d0f51276b5961c29028ae67195
somxr <60410055+somxr@users.noreply.github.com>
2025-08-02 23:49:25 -0400

    add music autoload

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -1,5 +1,16 @@
 extends Interactable
 
+@export var wiggle_amount: float = 0.1  # Radians
+@export var wiggle_speed: float = 0.5   # Seconds per direction
+
+func _ready():
+	start_wiggle()
+
+func start_wiggle():
+	var tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "rotation:y", wiggle_amount, wiggle_speed)
+	tween.tween_property(self, "rotation:y", -wiggle_amount, wiggle_speed)
+
 func interact(_body):
 	
 	if _body.has_method("collect_fish"):
